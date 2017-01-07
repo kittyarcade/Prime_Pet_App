eb5199d6bc398614dc3ed2c5a372c3d074c83ac4
Amanda Jopp <aclaire488@gmail.com>
2017-01-06 22:48:45 -0600

    button working, POST kind of working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,27 @@ var myApp = angular.module('myApp', []);
 myApp.controller('Hoth', ['$scope', '$http', function($scope, $http){
   console.log('totally angular dude');
 
+  $scope.submit = function(){
+    console.log('clicked submit');
+    var petAdded = {
+      Name: $scope.petName,
+      Animal: $scope.petType,
+      Age: $scope.petAge,
+      Image: $scope.petImg
+    };
+    console.log(petAdded);
+    $http({
+      method: 'POST',
+      url: '/router',
+      data: petAdded
+    }).then(function(response){
+      console.log('POST response: ', response);
+    });//end $http
+    $scope.petName='';
+    $scope.petType='';
+    $scope.petAge='';
+    $scope.petImg='';
+  }; //end $scope.submit
 
 
 
--- server/router/pets.js
+++ server/router/pets.js
@@ -3,7 +3,24 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var User = require('../models/newPet');
 
-
+router.post('/', function(req, res){
+  var info = req.body;
+  console.log('Object:', info);
+  var postPet = new User({
+    Name: info.Name,
+    Animal: info.Animal,
+    Age: info.Age,
+    Image: info.Image
+  });
+  postPet.save(function(err){
+    if(err){
+      console.log('POST: ', 500);
+    } else {
+      console.log('POST: ', 200);
+      res.send('pet added');
+    }
+  });
+});
 
 
 module.exports = router;
