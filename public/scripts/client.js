daec8c524e14549b851433c92b271adbc27a08c8
Amanda Jopp <aclaire488@gmail.com>
2017-01-06 21:32:00 -0600

    angular established and logging

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,8 @@
 console.log('meow.js');
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('Hoth', ['$scope', '$http', function($scope, $http){
+  console.log('totally angular dude');
+
+}]); //end hoth controller
